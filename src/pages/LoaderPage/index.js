6d9eacbde299d0ece29e8af76374a17c0aa327dd
kakDevonte <kakdevonte@gmail.com>
2022-09-18 20:49:43 +0700

    add random loader

--- src/pages/LoaderPage/index.js
+++ src/pages/LoaderPage/index.js
@@ -3,15 +3,20 @@ import styles from "./LoaderPage.module.scss";
 import { useNavigate } from "react-router-dom";
 import loader from "../../assets/loader.png";
 
+const randomIntFromInterval = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
 const LoaderPage = () => {
   const [uploadOrDownloadCount, setUploadOrDownloadCount] = React.useState(10);
   const navigate = useNavigate();
   React.useEffect(() => {
     const timer = setInterval(() => {
       setUploadOrDownloadCount((beforeValue) =>
-        beforeValue >= 100 ? navigate("/home") : beforeValue + 10
+        beforeValue >= 100
+          ? navigate("/home")
+          : beforeValue + randomIntFromInterval(5, 15)
       );
-    }, 150);
+    }, 100);
     return () => {
       clearInterval(timer);
     };
@@ -23,7 +28,7 @@ const LoaderPage = () => {
         На удачу
       </h1>
       <span className={styles.loader}>{`${Math.round(
-        uploadOrDownloadCount
+        uploadOrDownloadCount >= 100 ? 100 : uploadOrDownloadCount
       )}%`}</span>
       <img className={styles.img} src={loader} />
     </div>
